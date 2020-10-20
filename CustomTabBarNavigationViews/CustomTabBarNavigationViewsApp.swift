56425f0c2a9ab7c72c6a91ec6e289420618ae350
Max <maxim.macari.dam@gmail.com>
2020-10-20 17:56:30 +0200

    Initial Commit

---
+++ CustomTabBarNavigationViews/CustomTabBarNavigationViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarNavigationViewsApp.swift
+//  CustomTabBarNavigationViews
+//
+//  Created by Maxim Macari on 20/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarNavigationViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
